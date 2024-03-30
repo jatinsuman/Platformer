c60a8a48dee6c5f13d4fc4087c1598e2c29d2163
jatinsuman <jatinsuman2023@gmail.com>
2024-03-30 11:01:18 +0530

    basic platforms created

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import sys
+import random
 
 clock = pygame.time.Clock()
 
 HEIGHT = 600
 WIDTH = 900
 
-bgY = 0
+bgY = -144
 
 scroll = 0
 
@@ -20,17 +21,26 @@ for i in range(0, 4):
 
 
 def blit():
-    for x in range(5):
+    for x in range(20):
         speed = 1
         for image in bg_images:
             screen.blit(image, ((x * 950) + scroll * speed , bgY))
             speed += 0.08
+        
 
+left = 0
+rectangles = []
+
+lowWidth = 20
+highWidth = 250
+
+lowHeight = 20
+highHeight = 250
 
 running = True
 while running:
     clock.tick(60)
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -38,13 +48,31 @@ while running:
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] and scroll < 0 :
         scroll += 5
+        for rect in rectangles:
+            rect.move_ip(5, 0)
     if key[pygame.K_RIGHT]and scroll > -3850:
         scroll -= 5
+        for rect in rectangles:
+            rect.move_ip(-5, 0)
+    if key[pygame.K_DOWN] and bgY > -144:
+        bgY -= 3
 
-    
-    blit()
-    
-    
+    if left <=  900 - scroll:
+        widthadd = random.randrange(lowWidth, highWidth)
+        heightadd = random.randrange(lowHeight, highHeight)
+        rectangles.append(pygame.Rect(left, HEIGHT - heightadd, widthadd, heightadd))
+        highHeight = heightadd + 60
+
+
+    blit()    
+
+
+    for rect in rectangles:
+        pygame.draw.rect(screen, (0, 0, 0), rect)
+
+    if left <=  900 - scroll:
+        left += widthadd/2    
+        
     pygame.display.update()
 
 pygame.quit()
